aa28a04894fbd6cc9fe4e3c1be37f1702d3f16c4
maheshwarishikha <maheshwari.shikha@gmail.com>
2023-07-19 18:10:04 +0530

    Update main.tf

--- terraform-ibm-hpcs/examples/basic/main.tf
+++ terraform-ibm-hpcs/examples/basic/main.tf
@@ -1,7 +1,7 @@
 ##############################################################################
 # Resource Group
 ##############################################################################
-
+/*
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
   version = "1.0.5"
@@ -19,12 +19,12 @@ module "hpcs_instance" {
   resource_group_id                               = module.resource_group.resource_group_id
   auto_initialization_using_recovery_crypto_units = false
   service_endpoints = "public-and-private"
-}
+}*/
 
-/*
+
 module "kms_key_ring" {
   source      = "./terraform-ibm-kms-key-ring"
   endpoint_type = "public"
-  instance_id = module.hpcs_instance.guid
+  instance_id = "9a0d600d-833a-4a9e-800c-6f55107c163d" #module.hpcs_instance.guid
   key_ring_id = "${var.prefix}-key-ring"
-} */
+} 
